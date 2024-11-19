591d89b05bdf2c8791f9f41073fd18612c7793f0
samir9187 <samirkunwar9187@gmail.com>
2024-11-20 00:02:04 +0530

    deploy issue resolved

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://todo-assignment-xi.vercel.app/",
+    origin: "https://todo-site-c487be.netlify.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Authorization",
     credentials: true,
